cbbf60d630f1932c80b8f100b9100a9ffa4d39d7
souvik-websadroit <souvik.websadroit@gmail.com>
2026-01-13 11:21:14 +0000

    LinkBTn CSS

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,17 @@
-export default function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disabled, to }) {
+  const className =
+    "md inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
   return (
-    <button
-      disabled={disabled}
-      className="md inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
