d41cdba2559982e6baeb0292f9ec86906151ca0e
souvik-websadroit <souvik.websadroit@gmail.com>
2026-01-14 13:32:23 +0000

    Menu CSS ADD

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to }) {
-  const className =
-    "md inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4";
+export default function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
