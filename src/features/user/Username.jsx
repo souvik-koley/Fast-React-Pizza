43a17ea40f642db3aefb792ccc32a0f58d32199f
Souvik Koley <souvikkoley9674@gmail.com>
2026-01-18 20:27:10 +0530

    User State Add Using Redux Toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Souvik</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
